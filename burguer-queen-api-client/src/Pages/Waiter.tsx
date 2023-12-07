0f1e3eb146749feb1fdfbbaa2736f069aec8e5ea
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-12-07 09:02:54 -0600

    Creacod rutas de waiter y login

---
+++ burguer-queen-api-client/src/Pages/Waiter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export interface IWaiterProps {}
+
+const WaiterPage: React.FunctionComponent<IWaiterProps> = (props) => {
+    return (
+        <div>
+            <p>This is the Waiter page</p>
+        </div>
+    )
+}
+
+export default WaiterPage;
